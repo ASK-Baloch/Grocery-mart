c45dd87d57f0bf475f2819b315bb98f228854128
Ahmed <balochsaeed888@gmail.com>
2024-08-01 18:02:29 +0500

    changed the primary color from neutral to green

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,11 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1>Hello World</ h1>
+    <div className="flex justify-center gap-2 items-center absolute left-52 top-52">
+      <h1>Hello World</h1>
+      <Button>HUI HUI</Button>
+    </div>
   );
 }
